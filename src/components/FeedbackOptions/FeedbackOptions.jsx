40f48f9a1c5a72a44a688d461082d6c962d11645
lewypopescu <popesculewy@gmail.com>
2024-07-19 19:14:59 +0200

    first 2 functions done

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,25 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 // import styles from './FeedbackOptions.module.css';
-
-class FeedbackOptions extends Component {
-  render() {
-    const { options, onLeaveFeedback } = this.props;
-    return (
-      <div>
-        {options.map(option => (
-          <button key={option} onClick={() => onLeaveFeedback(option)}>
-            {option.charAt(0).toUpperCase() + option.slice(1)}
-          </button>
-        ))}
-      </div>
-    );
-  }
+export default function FeedbackOptions({ options, onLeaveFeedback }) {
+  return (
+    <div>
+      {options.map(option => (
+        <button key={option} onClick={() => onLeaveFeedback(option)}>
+          {option.chart(0).toUpperCase() + option.slice(1)}
+        </button>
+      ))}
+    </div>
+  );
 }
 
 FeedbackOptions.propTypes = {
   options: PropTypes.arrayOf(PropTypes.string).isRequired,
   onLeaveFeedback: PropTypes.func.isRequired,
 };
-
-export default FeedbackOptions;
--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -1,16 +1,10 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 // import styles from './Notification.module.css';
-
-class Notification extends Component {
-  render() {
-    const { message } = this.props;
-    return <p>{message}</p>;
-  }
+export default function Notification({ message }) {
+  return <p>{message}</p>;
 }
 
 Notification.propTypes = {
   message: PropTypes.string.isRequired,
 };
-
-export default Notification;
